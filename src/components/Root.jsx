6cc433a1312bccbf674c6bdc6e217687220d3ee9
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-01-16 21:36:22 +0600

    Adding Router Setup

---
+++ src/components/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header and footer/Header';
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/routes/router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../components/Root";
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <Root></Root>,
+        children:[
+            
+        ]
+    },
+])
